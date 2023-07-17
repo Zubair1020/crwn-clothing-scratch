b6d27c532ab468757a6113f97badf24f3f4a763e
Zubair1020~ <rakibhossianraju.com>
2023-07-17 18:07:16 +0600

    " "

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -25,7 +25,7 @@ const PaymentForm = () => {
 
     try {
       const { paymentIntent } = await fetch(
-        "/.netlify/functions/create-payment-intent",
+        ".netlify/functions/create-payment-intent",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
